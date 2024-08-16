df7fa23cd6baf1b0ac05380629a706395b2024d0
Asad Ullah <153418628+aizaz88@users.noreply.github.com>
2024-08-16 13:19:29 +0500

    Time: 2 ms (89.40%) | Memory: 41.7 MB (50.50%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String op : operations) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top); 
+                stack.push(newTop);
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+        
+        int sum = 0;
+        for (int score : stack) {
+            sum += score;
+        }
+        
+        return sum;
+    }
+}
