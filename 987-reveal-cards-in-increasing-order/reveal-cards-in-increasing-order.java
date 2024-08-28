942e1da57d0fa462eae8e6d73564e061f9ec5406
Asad Ullah <153418628+aizaz88@users.noreply.github.com>
2024-08-28 18:59:59 +0500

    Time: 4 ms (80.14%) | Memory: 43.6 MB (54.09%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        
+        int n=deck.length;
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            q.add(i);
+        }
+        Arrays.sort(deck);
+        
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            ans[q.poll()]=deck[i];
+            
+            if(!q.isEmpty()){
+                q.add(q.poll());
+            }
+        }
+        return ans;
+    }
+}
