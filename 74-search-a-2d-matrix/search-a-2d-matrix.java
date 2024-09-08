e5c6232f709a042f28598c7362183f975066bc0d
Asad Ullah <153418628+aizaz88@users.noreply.github.com>
2024-09-08 13:06:19 +0500

    Time: 0 ms (100.00%) | Memory: 42 MB (52.30%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,7 +1,29 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
+        //Binary search algo (time complexity O(logm*n))
         int m=matrix.length;
+        int n=matrix[0].length;
+        
+        int start=0;
+        int end=m*n-1;
+        
+        while(start<=end){
+            int mid=(start+end)/2;
+            int midvalue=matrix[mid/n][mid%n];
+            if(midvalue==target){
+                return true;
+            }else if(midvalue<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return false;
+        
+        /*
+        //Linear search algo(time complexity O(m x n))
+          int m=matrix.length;
         int n=matrix[0].length;
         
         for(int i=0;i<m;i++){
@@ -12,5 +34,6 @@ class Solution {
             }
         }
         return false;
+        */
     }
 }
