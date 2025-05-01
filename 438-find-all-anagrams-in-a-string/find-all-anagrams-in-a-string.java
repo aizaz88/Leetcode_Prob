e25ed9b35a10c0bf23629afd26521b93381e505a
Asad Ullah <153418628+aizaz88@users.noreply.github.com>
2025-05-01 11:38:47 +0500

    Time: 10 ms (79.74%) | Memory: 44.8 MB (72.73%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,35 +1,30 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+            List<Integer> list=new ArrayList<>();
+            if(s.length()<p.length()) return list;
 
-        List<Integer> list=new ArrayList<>();
-        if (s.length() < p.length()) return list;
-        
-        int[] pCharFreq=new int[26];
-        int[] sCharFreq=new int[26];
-
-        for(char c:p.toCharArray()){
-            pCharFreq[c-'a']++;
-        }
-
-    int window=p.length();
-    for(int i=0;i<window;i++){
-        sCharFreq[s.charAt(i)-'a']++;
-    }
-
-    if(Arrays.equals(pCharFreq,sCharFreq)){
-        list.add(0);
-    }
-
-
-        for(int i=window;i<s.length();i++){
-            sCharFreq[s.charAt(i-window)-'a']--;
-
-            sCharFreq[s.charAt(i)-'a']++;
-
-            if(Arrays.equals(pCharFreq,sCharFreq)){
-                list.add(i-window+1);
+            int[] pCharFreq=new int[26];
+            int[] sCharFreq=new int[26];
+            int window=p.length();
+            
+            for(char c:p.toCharArray()){
+                pCharFreq[c-'a']++;
             }
-        }
-        return list;
+
+            for(int i=0;i<window;i++){
+                sCharFreq[s.charAt(i)-'a']++;
+            }
+
+            //1st Window check 
+            if(Arrays.equals(sCharFreq,pCharFreq)) list.add(0);
+
+            for(int i=window;i<s.length();i++){
+                sCharFreq[s.charAt(i-window)-'a']--;
+                sCharFreq[s.charAt(i)-'a']++;
+
+                if(Arrays.equals(sCharFreq,pCharFreq)) list.add(i-window+1);
+            }
+
+            return list;
     }
 }
