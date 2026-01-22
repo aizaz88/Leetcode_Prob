76e837d7676bb9f5b1f791a9adcadecaee1f2d5e
Asad Ullah <153418628+aizaz88@users.noreply.github.com>
2026-01-22 13:29:01 +0500

    Time: 2 ms (64.76%) | Memory: 43.2 MB (18.54%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isFascinating(int n) {
-        
-        String str=String.valueOf(n)+String.valueOf(2*n)+String.valueOf(3*n);
-        
-         if(str.length()!=9){
-            return false;
-        }
-        
-        Set<Character> digitset=new HashSet<>();
-        
-        for(char ch:str.toCharArray()){
-            if(ch=='0' || !digitset.add(ch)){
+        String s1= String.valueOf(n);
+        String s2= String.valueOf(2*n);
+        String s3= String.valueOf(3*n);
+
+        StringBuilder sb=new StringBuilder();
+        sb.append(s1).append(s2).append(s3);
+
+        int[] occ=new int[10];
+        for(char ch:sb.toString().toCharArray()){
+            if(ch=='0'){
                 return false;
+            }else{
+                occ[ch-48]++;
+                if(occ[ch-48]>1){
+                    return false;
+                }
             }
         }
-        
-      return digitset.size()==9;
+            return true;
     }
 }
