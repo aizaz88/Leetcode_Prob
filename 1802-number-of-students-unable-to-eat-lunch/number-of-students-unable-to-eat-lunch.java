6ab8a1c2be8d8e5faa56da8435c294dc7729997d
Asad Ullah <153418628+aizaz88@users.noreply.github.com>
2026-02-02 22:46:42 +0500

    Time: 2 ms (43.36%) | Memory: 43.4 MB (33.64%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -1,24 +1,24 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         Queue<Integer> q=new LinkedList<>();
-        
+
         for(int i=0;i<students.length;i++){
             q.add(students[i]);
         }
 
-           int idx=0;
-           int count=0;
-
-            while(!q.isEmpty() && count<q.size()){
-                if(q.peek()==sandwiches[idx]){
-                    q.poll();
-                    idx++;
-                    count=0;
-                }else{
-                    q.add(q.poll());
-                    count++;
-                }
+        int idx=0;
+        int count=0;
+     
+        while(count<q.size() && !q.isEmpty()){
+            if(q.peek()==sandwiches[idx]){
+                q.poll();
+                idx++;
+                count=0;
+            }else{
+                q.offer(q.poll());
+                count++;
             }
-            return q.size();
+        }
+        return q.size();
     }
 }
